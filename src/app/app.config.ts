6a4aeeb7e3f60c45dd5a4f2be73b30cbba7f5ebb
Scarounine <paulinelesueur93@gmail.com>
2025-11-07 15:49:31 +0100

    feat(firebase): initialize firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,27 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp(
+      { 
+        projectId: "country-saloon-82-aba94", 
+        appId: "1:1013592832855:web:2946f125f35fbdddd05c12", 
+        storageBucket: "country-saloon-82-aba94.firebasestorage.app", 
+        apiKey: "AIzaSyDKFHM5G6shLZnAe-2xdXYTNobeyQCuiSk", 
+        authDomain: "country-saloon-82-aba94.firebaseapp.com", 
+        messagingSenderId: "1013592832855", 
+        measurementId: "G-BSH9XW2P2D" 
+      }
+    )), 
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyDKFHM5G6shLZnAe-2xdXYTNobeyQCuiSk",
+        authDomain: "country-saloon-82-aba94.firebaseapp.com",
+        projectId: "country-saloon-82-aba94",
+        storageBucket: "country-saloon-82-aba94.firebasestorage.app",
+        messagingSenderId: "1013592832855",
+        appId: "1:1013592832855:web:2946f125f35fbdddd05c12",
+        measurementId: "G-BSH9XW2P2D"
+    }
+};
