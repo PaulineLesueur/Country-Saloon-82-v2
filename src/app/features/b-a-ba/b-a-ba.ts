01f51fbb8b1255d5614b5d5fe5ca783404f68a2a
Scarounine <paulinelesueur93@gmail.com>
2025-11-12 12:57:25 +0100

    feat(b-a-ba): add content

--- src/app/features/b-a-ba/b-a-ba.ts
+++ src/app/features/b-a-ba/b-a-ba.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-b-a-ba',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,TabsModule],
   templateUrl: './b-a-ba.html',
   styleUrl: './b-a-ba.sass',
 })
 export class BABa implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 }
